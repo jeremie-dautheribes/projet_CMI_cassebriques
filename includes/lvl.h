f965a1917f9aaf8432fc4c9cf5c465e56072ceea
jeremie-dautheribes <je.je.laeti@gmail.com>
2016-04-11 14:53:34 +0200

    suppression de lvl"1"

--- includes/lvl.h
+++ includes/lvl.h
@@ -1,5 +1,5 @@
-#ifndef LVL1_H_INCLUDED
-#define LVL1_H_INCLUDED
+#ifndef LVL_H_INCLUDED
+#define LVL_H_INCLUDED
 
 void lvl1(sf:: Text &text, sf::RenderWindow &window, sf::Texture &stop, sf::Sprite &sprite,sf::CircleShape &circle,sf::RectangleShape &rectangle,sf::RectangleShape &Briques, sf::RectangleShape &bord, bool &pause, int** brique, bool &perd,int &niveau);
 void lvl2(sf:: Text &text,sf::RenderWindow &window, sf::Texture &stop, sf::Sprite &sprite,sf::CircleShape &circle,sf::RectangleShape &rectangle,sf::RectangleShape &Briques, sf::RectangleShape &bord, bool &pause, int** brique, bool &perd, int &niveau);
