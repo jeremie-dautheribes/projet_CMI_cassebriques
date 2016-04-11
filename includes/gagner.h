70cc11c50b684596374aca5f3afffb8da36c0ccc
ThomasBertin <thomas.bertin.p@gmail.com>
2016-04-11 23:00:26 +0200

    Delete gagner.h

--- includes/gagner.h
+++
@@ -1,7 +0,0 @@
-#ifndef GAGNER_H_INCLUDED
-#define GAGNER_H_INCLUDED
-
-void menugagner (sf::RenderWindow &gagner);
-void gagner(sf::RenderWindow &window, int** brique, int nbLignes, int nbColonnes);
-
-#endif // GAGNER_H_INCLUDED
