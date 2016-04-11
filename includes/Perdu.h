ac3c86552494247922ca1f9d28d098c50d43ddce
ThomasBertin <thomas.bertin.p@gmail.com>
2016-04-11 23:00:06 +0200

    Delete Perdu.h

--- includes/Perdu.h
+++
@@ -1,7 +0,0 @@
-#ifndef PERDU_H_INCLUDED
-#define PERDU_H_INCLUDED
-
-void menuperdu (sf::RenderWindow &fenetre,sf::RenderWindow &window,int **brique);
-void perdu (sf::Event &event,sf::Sprite &Sbackground,sf::RenderWindow &window, sf::RectangleShape &rectangle,sf::RectangleShape &bord, sf::CircleShape &circle, sf::RectangleShape &Briques, int** brique, int &lives, float& xBalle, float& yBalle, float &vitX, float &vitY, bool &perd, float &rectX,float &MouseX);
-
-#endif // PERDU_H_INCLUDED
