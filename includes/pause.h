6ec34ec06d4ba3a2204d21e16228c9aa638b0e96
ThomasBertin <thomas.bertin.p@gmail.com>
2016-04-11 23:00:36 +0200

    Delete pause.h

--- includes/pause.h
+++
@@ -1,6 +0,0 @@
-#ifndef PAUSE_H_INCLUDED
-#define PAUSE_H_INCLUDED
-
-void menuPause (sf::Texture &stop, sf::Sprite &sprite, sf::RenderWindow &window, bool &pause, sf::CircleShape &circle);
-
-#endif // PAUSE_H_INCLUDED
