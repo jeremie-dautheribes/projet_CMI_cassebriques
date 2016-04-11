55230e8ff4f509f046c7d81536824cdac083756b
ThomasBertin <thomas.bertin.p@gmail.com>
2016-04-11 23:00:16 +0200

    Delete briques.h

--- includes/briques.h
+++
@@ -1,8 +0,0 @@
-#ifndef BRIQUES_H_INCLUDED
-#define BRIQUES_H_INCLUDED
-
-void initialisationBrique (int** tab, int nbLignes, int nbColonnes );
-void disparitonRebondSurBrique (int** brique,float &x, float &y,float &vitX, float &vitY, int nbLignes, int nbColonnes);
-void creationBrique(sf::RenderWindow &window,sf::RectangleShape Briques, int **brique, int nbLignes, int nbColonnes);
-
-#endif // BRIQUES_H_INCLUDED
